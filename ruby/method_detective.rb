3cdd1d920b5434ca5ee6f936fb200f26ceafc723
Davis Bare <davisbare409@gmail.com>
2016-10-31 12:38:22 -0700

    fishing the cases

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,47 @@
 puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
+
 puts"zom".chomp("o")
 # => “zoom”
 
-puts"enhance".rjust(10)
+
+puts"enhance".rjust(15)
+puts"enhance".center(20)
 # => "    enhance    "
+
 
 puts"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+
+string_1 = "the usual"
+puts string_1 << " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+
+puts" suspects".prepend("the usual")
 # => "the usual suspects"
 
+
+string_2 = "The case of the disappearing last letter"
+
 puts"The case of the disappearing last letter".chop
+puts string_2[0...37]
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+
+string_3 = "The mystery of the missing first letter"
+puts string_3[1...50]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".each_byte{|z|print z}
+puts "z".each_codepoint{|z| print z}
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
