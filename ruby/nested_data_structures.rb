b57a764e2e4fa4e8cdbfb293297a7818a7597828
Davis Bare <davisbare409@gmail.com>
2016-11-02 13:20:49 -0700

    working with nested data sets

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,59 @@
+
+
+overwatch = {
+  attack: {
+      type: 'dps',
+      team_info: {
+          total_roster_spots: 6,
+          dps_spots: 2
+      },
+      meals: [
+          'Mcree',
+          'Reaper',
+          'Genji'
+      ]
+  },
+  defense: {
+      type: 'aoe',
+      team_info: {
+          total_roster_spots: 6,
+          aoe_spots: 2
+      },
+      heros: [
+          'Junkrat',
+          'Mei',
+          'Hanzo'
+      ]
+  },
+  tank: {
+      type: 'tank',
+      team_info: {
+          total_roster_spots: 6,
+          tank_spots: 2
+      },
+      heros: [
+      		'Roadhog',
+      		'Zarya',
+        	'D.Va'
+      ]
+  },
+  support: {
+  	  type: 'healer',
+  	  team_info: {
+  	  	  total_roster_spots: 6,
+  	  	  healer_spots: 2
+  	  },
+  	  heros: [
+  	  		'Lucio',
+  	  		'Mercy',
+  	  		'Ana'
+  	  ]
+  }
+}
+
+p overwatch[:defense][:heros][2]
+p overwatch[:support][:heros].push('Zenyatta')
+p overwatch[:tank][:heros][3] = 'Reinhardt'
+
+
+
