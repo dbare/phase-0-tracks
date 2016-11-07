e08f03c917c1b6f048e69636419b79fd2ee4c23c
Davis Bare <davisbare409@gmail.com>
2016-11-07 11:41:48 -0800

    added class layout to puppies file

---
+++ ruby/puppies.rb
@@ -0,0 +1,16 @@
+=begin
+SPECIEIS-------------------
+Siberian Husky
+
+CHARACTERISTICS------------
+Life Span: 12 - 15yrs
+Height: 20 - 25in
+Colors: varies
+
+BEHAVIOR-------------------
+Outgoing
+Gentle
+Friendly
+Alert
+Intellegent
+=end
