c0b5e81c522290071376c015b9438e5b80417247
Davis Bare <davisbare409@gmail.com>
2016-11-08 14:41:27 -0800

    complete 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -19,6 +19,10 @@ class Puppy
   end
   def eat(food)
   	puts "I love eating #{food}!"
+  	puts
+  end
+  def initialize
+  	print "initializing new puppy instance..."
   end
 end
 
@@ -28,3 +32,40 @@ scooby.speak(3)
 scooby.roll_over
 scooby.dog_years(14)
 scooby.eat("pizza")
+
+
+class Futurama_Aliens
+	def initialize
+		puts "initializing alien race....."
+	end
+	def intro(name)
+		puts "Hi! My name is #{name}!"
+	end
+	def location(planet,distance)
+		puts "I am from #{planet}, #{distance} light years away."
+	end
+end
+
+alien = []
+chars = ['Bender', 'Fry', 'Leela', 'Scruffy', 'Dr. Zoidburg', 'Amy', 'Calculon']
+
+#our method
+
+50.times do |i|
+	alien[i] = Futurama_Aliens.new
+	alien[i].intro(chars.sample)
+	alien[i].location('earth', 100)
+end
+
+p alien.length
+
+#alternative method
+
+alien.each do |char_name|
+	char_name.intro(chars.sample)
+	char_name.location('mars', 0.2)
+end
+
+
+
+
