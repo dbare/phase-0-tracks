84179e1e5944b9ed38b539a9f99ead4e143fa179
Davis Bare <davisbare409@gmail.com>
2016-11-10 14:20:47 -0800

    finishing 6.5 paring session

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,32 +1,57 @@
 class Santa
 
 	def initialize(gender,ethnicity)
+		puts 'initializing Santa instance........'
 		@gender = gender
 		@ethnicity = ethnicity
-		puts 'initializing Santa instance........'
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
-
 	def speak
 		puts 'Ho, ho ,ho! Haaaaappy holidays!'
 	end
-	
 	def eat_milk_and_cookies(cookie_type)
 		puts 'That was a good ' + cookie_type +' cookie!'
 	end
+
+
+	def celebrate_birthday=(current_age)
+		current_age = @age + 1
+	end
+	def get_mad_at(reindeer_name)
+
+	end
+	def gender=
+		@gender
+	end
+	def age
+		@age +=1
+		@age
+	end
+	def ethnicity
+		@ethnicity
+	end
 end
 
-age = 0
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+santa = Santa.new(' ',' ')
 
-all_santas = []
-all_santas << Santa.new('male', 'black')
-
-all_santas.each do |santa|
-	santa.speak
-	santa.eat_milk_and_cookies('chocolate chip')
-end
+santa.speak
+santa.eat_milk_and_cookies('Oatmeal')
+santa.celebrate_birthday(0)
+#Release 1
+=begin
+santas = []
 
 
-#Saint_Nick = Santa.new('male', 'african-american')
-#Saint_Nick.speak
-#Saint_Nick.eat_milk_and_cookies('Oatmeal Raisin')
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+puts santas
+=end
+
+
