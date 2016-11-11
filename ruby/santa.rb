6cc236d6ee1b91167ef225bb890cf12dd1ca4e29
Davis Bare <davisbare409@gmail.com>
2016-11-11 08:08:55 -0800

    finishing sant.rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,43 +1,53 @@
-class Santa
+number_of_santas = 0
 
-	def initialize(gender,ethnicity)
-		puts 'initializing Santa instance........'
-		@gender = gender
-		@ethnicity = ethnicity
-		@age = 0
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
-	def speak
-		puts 'Ho, ho ,ho! Haaaaappy holidays!'
-	end
-	def eat_milk_and_cookies(cookie_type)
-		puts 'That was a good ' + cookie_type +' cookie!'
-	end
+until number_of_santas == 50 do
+	class Santa
+		attr_accessor :age, :ethnicity, :gender
 
 
-	def celebrate_birthday=(current_age)
-		current_age = @age + 1
-	end
-	def get_mad_at(reindeer_name)
+		def initialize(gender,ethnicity)
+			example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+			example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
+			puts 'initializing Santa instance........'
+			@gender = example_genders[rand(example_genders.length)]
+			@ethnicity = example_ethnicities[rand(example_ethnicities.length)]
+			@age = rand(1..140)
+			@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		end
+		def speak
+			puts 'Ho, ho ,ho! Haaaaappy holidays!'
+		end
+		def eat_milk_and_cookies(cookie_type)
+			puts 'That was a good ' + cookie_type +' cookie!'
+		end
+		def celebrate_birthday=(current_age)
+			@celebrate_birthday = current_age + 1
+			p @celebrate_birthday
+		end
+		def get_mad_at=(reindeer_name)
+			@get_mad_at = @reindeer_ranking.last(reindeer_name)
+			p @get_mad_at
+		end
 	end
-	def gender=
-		@gender
-	end
-	def age
-		@age +=1
-		@age
-	end
-	def ethnicity
-		@ethnicity
-	end
-end
+	puts 'This Santa is #{@age} years old and loves being santa.'
+	puts 'They are #{@ethnicity}.'
+	puts 'When it comes to gender they are: #{@gender}'
+	number_of_santas += 1
+end 
 
 santa = Santa.new(' ',' ')
 
-santa.speak
-santa.eat_milk_and_cookies('Oatmeal')
-santa.celebrate_birthday(0)
+#santa.speak
+#santa.eat_milk_and_cookies('Oatmeal')
+#santa.celebrate_birthday = 0
+#p santa.age
+#p santa.ethnicity
+#p santa.gender
+
+
+
+
 #Release 1
 =begin
 santas = []
