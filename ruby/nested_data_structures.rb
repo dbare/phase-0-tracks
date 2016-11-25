4ef794ca6afeeeee34563f6dd7d362569e7de8d0
Davis Bare <davisbare409@gmail.com>
2016-11-25 11:01:47 -0800

    added the nested structures to file

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,2 +1,30 @@
+football_team = {
+  offense: {
+    type: 'skill positions',
+    team_info: {
+      total_roster_spots: 11,
+      num_skill_positions: 4
+    }
+    positions: [
+      'Running Back',
+      'Wide Reciever',
+      'Wide Reciever',
+      'Quarterback']
+
+  }
+  defense: {
+    type: 'skill positions',
+    team_info: {
+      total_roster_spots: 11,
+      num_skill_positions: 4
+    }
+    positions: [
+      'Cornerback',
+      'Cornerback',
+      'Free Safety',
+      'Strong Safety']
+  }
+}
+
 
 
