96c47607caf7dc31b21d8abb39a02a4dbfb78970
Davis Bare <davisbare409@gmail.com>
2016-11-25 11:08:57 -0800

    added driver code for nest structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,27 +4,27 @@ football_team = {
     team_info: {
       total_roster_spots: 11,
       num_skill_positions: 4
-    }
+    },
     positions: [
       'Running Back',
       'Wide Reciever',
-      'Wide Reciever',
-      'Quarterback']
-
-  }
+      'Quarterback'
+    ]
+  },
   defense: {
     type: 'skill positions',
     team_info: {
       total_roster_spots: 11,
       num_skill_positions: 4
-    }
+    },
     positions: [
       'Cornerback',
       'Cornerback',
-      'Free Safety',
-      'Strong Safety']
+      'Free Safety'
+    ]
   }
 }
 
-
-
+p football_team[:offense][:positions][0]
+p football_team[:offense][:positions].push('Wide Reciever')
+p football_team[:defense][:positions][3] = 'Strong Safety'
