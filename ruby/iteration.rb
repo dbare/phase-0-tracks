eba5873b862ca43b2b5f179df60e92a1f02ea040
Davis Bare <davisbare409@gmail.com>
2016-11-25 10:39:27 -0800

    added sample array and hash to demonstrate .map methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,99 +1,41 @@
-def my_method
+def a_method
   variable = "\"Hello, World\""
-  puts "The block has not run yet."
+  puts "this block won't run yet"
   yield(variable)
-  puts "The block has run."
+  puts "the block will now run"
 end
 
-my_method { |variable| puts "When a computer wakes up, it says #{variable}" }
+a_method {|variable| puts "When the computer wakes up it says #{variable}"}
 
-oscar_categories = ["Best Picture", "Best Editing", "Best Director"]
-
-famous_roles = {
-  George_Clooney: "Danny Ocean",
-  Keanu_Reeves: "Neo"
+award_categories = ["Best Game", "Best Stroy", "Best Company"]
+character_roles = {
+  Cloud: "Final Fantasy VII",
+  Geralt: "The Witcher"
 }
 
-oscar_categories.each do |category|
+award_categories.each do |category|
   puts category
 end
 
-famous_roles.each do |actor, role|
-  puts "#{actor} played #{role}"
+character_roles.each do |character, game|
+  puts "#{character} was in the game #{game}"
 end
 
-puts "Pre-map data structures:"
-p oscar_categories
-p famous_roles
+puts "data structures before map"
+p award_categories
+p character_roles
 
-oscar_categories.map! do |category|
-  category.upcase
-end
+##############
 
-puts "Post-map data structures:"
-p oscar_categories
-p famous_roles
-
-#########################
-
-# Declare sample array and hash
-sample_ary = ["a", "b", "c", "d", "e"]
-sample_hash = {
-  sky: "blue",
-  sea: "green",
-  trees: "tall",
-  dirt: "dirty"
+#simple array and hash
+array = ['a','b','c','d']
+hash = {
+  grass: 'green',
+  ocean: 'blue',
+  wind: 'loud',
+  rain: 'wet'
 }
 
-# Print the data before they are modified to verify
-puts "Pre-modified data:"
-p sample_ary
-p sample_hash
-
-# Print the modified data to see how it changes the array and hash.
-puts "Modified data: #delete_if"
-p sample_ary.delete_if { |letter| letter > "d" }
-p sample_hash.delete_if { |key, value| value == "dirty"}
-
-# .delete_if is a destructive method, so re-declaring the same array and hash
-# from lines 40-46
-sample_ary = ["a", "b", "c", "d", "e"]
-sample_hash = {
-  sky: "blue",
-  sea: "green",
-  trees: "tall",
-  dirt: "dirty"
-}
-
-# Print the modified data to see how it changes the array and hash.
-puts "Modified data: #keep_if"
-p sample_ary.keep_if { |letter| letter > "d" }
-p sample_hash.keep_if { |key, value| value == "dirty" }
-
-
-# .keep_if is a destructive method, so re-declaring the same array and hash
-# from lines 40-46
-sample_ary = ["a", "b", "c", "d", "e"]
-sample_hash = {
-  sky: "blue",
-  sea: "green",
-  trees: "tall",
-  dirt: "dirty"
-}
-
-# Print the modified data to see how it changes the array and hash.
-puts "Modified data: #select"
-p sample_ary.select { |letter| letter > "d" }
-p sample_hash.select { |key, value| value == "dirty" }
-
-puts "Modified data: #reject"
-p sample_ary.reject { |letter| letter > "d" }
-p sample_hash.reject { |key, value| value == "dirty" }
-
-# .select and .reject are not destructive methods so we do not need to 
-# declare the sample array an hash again.
-
-puts "Modified data: #drop_while"
-p sample_ary.drop_while { |letter| letter < "d"}
-# could not find an equivalent method for a hash
-
+#before they are modified
+p array
+p hash
