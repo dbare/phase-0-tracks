409ca3d6f79c47a99dfce572c5f52f7c9179e718
Davis Bare <davisbare409@gmail.com>
2016-11-25 10:11:43 -0800

    created hash with user info

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,3 +4,24 @@
 # - after questions are answered give the user a chance to update a key
 # - print the lastest updated hash
 # - exit program
+
+puts 'What is your name?'
+name = gets.chomp
+
+puts 'What is your age?'
+age = gets.chomp
+
+puts 'How many children do you have?'
+children_num = gets.chomp
+
+puts 'What theme would you like?'
+theme = gets.chomp
+
+user_info = {
+	name: name,
+	age: age,
+	children_num: children_num,
+	theme: theme,
+}
+
+p user_info
