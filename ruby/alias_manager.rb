6eb1abf5e685e744daba60d1d9e27e352f629ed6
Davis Bare <davisbare409@gmail.com>
2016-11-26 11:27:39 -0800

    finised second method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,3 +13,15 @@ def swap_names(name)
 	end
 	name_new.join
 end
+def costonant_adv(name)
+	consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+	name = name.split('')
+	name_new = name.map do |letter|
+		if consonants.include?(letter)
+			consonants.rotate[consonants.index(letter)]
+		else
+			letter
+		end
+	end
+	name_new.join
+end 
