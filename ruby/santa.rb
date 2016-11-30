5452a0ce9a3733f0fe1d4f1277e9bbcdd56cae02
Davis Bare <davisbare409@gmail.com>
2016-11-30 14:13:19 -0800

    added age, ranking, gender and ethnicity instance variables

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,67 +1,21 @@
-number_of_santas = 0
+class Santas
 
-until number_of_santas == 50 do
-	class Santa
-		attr_accessor :age, :ethnicity, :gender
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@age = 0
 
-
-		def initialize(gender,ethnicity)
-			example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-			example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-
-			puts 'initializing Santa instance........'
-			@gender = example_genders[rand(example_genders.length)]
-			@ethnicity = example_ethnicities[rand(example_ethnicities.length)]
-			@age = rand(1..140)
-			@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		end
-		def speak
-			puts 'Ho, ho ,ho! Haaaaappy holidays!'
-		end
-		def eat_milk_and_cookies(cookie_type)
-			puts 'That was a good ' + cookie_type +' cookie!'
-		end
-		def celebrate_birthday=(current_age)
-			@celebrate_birthday = current_age + 1
-			p @celebrate_birthday
-		end
-		def get_mad_at=(reindeer_name)
-			@get_mad_at = @reindeer_ranking.last(reindeer_name)
-			p @get_mad_at
-		end
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Initializing Santa instance...."
 	end
-	puts 'This Santa is #{@age} years old and loves being santa.'
-	puts 'They are #{@ethnicity}.'
-	puts 'When it comes to gender they are: #{@gender}'
-	number_of_santas += 1
-end 
+	def speak
+		puts "Ho, ho, ho! Happy holidays!"
+	end
+	def eat_milk_and_cookies(flavor)
+		puts "That was a good #{flavor} cookie!"
+	end
+end
 
-santa = Santa.new(' ',' ')
-
-#santa.speak
-#santa.eat_milk_and_cookies('Oatmeal')
-#santa.celebrate_birthday = 0
-#p santa.age
-#p santa.ethnicity
-#p santa.gender
-
-
-
-
-#Release 1
-=begin
-santas = []
-
-
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-
-puts santas
-=end
-
-
+santas = Santas.new("male", "black")
+santas.speak
+santas.eat_milk_and_cookies("Oatmeal Raisin")
