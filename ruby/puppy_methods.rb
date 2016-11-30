59adf8297a75a974260e561c57b510949b16ff8c
Davis Bare <davisbare409@gmail.com>
2016-11-30 10:36:18 -0800

    finished release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -31,3 +31,28 @@ scooby.speak(3)
 scooby.roll_over
 scooby.dog_years(14)
 scooby.eat("pizza")
+
+#created class
+
+class Lucio
+  def initialize
+    puts "initializing hero 'Lucio'.... booting up sound system"
+  end
+  def heal(hero)
+    puts "Time to heal #{hero}!"
+  end
+  def sound_barrier
+    puts "*activates sound barrirer*"
+  end
+end
+
+heal_heros = []
+heros = ['Reinhardt', 'Zarya', 'D.Va', 'Roadhog', 'Winston']
+
+50.times do |i|
+  heal_heros[i] = Lucio.new
+  heal_heros[i].heal(heros.sample)
+  heal_heros[i].sound_barrier
+end 
+
+p heal_heros.length
