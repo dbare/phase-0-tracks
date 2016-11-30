3c6b8df690c6cc868ebe0e5858f28130460b0cf2
Davis Bare <davisbare409@gmail.com>
2016-11-30 10:12:58 -0800

    release 0 finished

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,24 +5,24 @@ class Puppy
     toy
   end
   def speak(num)
-  	num.times{print "woof "}
-  	puts
+    num.times{print "woof "}
+    puts
   end
   def roll_over
-  	print "roll over"
-  	puts
+    print "roll over"
+    puts
   end
   def dog_years(year_old)
-  	dog_years = year_old/7
-  	print dog_years
-  	puts
+    dog_years = year_old/7
+    print dog_years
+    puts
   end
   def eat(food)
-  	puts "I love eating #{food}!"
-  	puts
+    puts "I love eating #{food}!"
+    puts
   end
   def initialize
-  	print "initializing new puppy instance..."
+    print "initializing new puppy instance..."
   end
 end
 
@@ -32,40 +32,3 @@ scooby.speak(3)
 scooby.roll_over
 scooby.dog_years(14)
 scooby.eat("pizza")
-
-
-class Futurama_Aliens
-	def initialize
-		puts "initializing alien race....."
-	end
-	def intro(name)
-		puts "Hi! My name is #{name}!"
-	end
-	def location(planet,distance)
-		puts "I am from #{planet}, #{distance} light years away."
-	end
-end
-
-alien = []
-chars = ['Bender', 'Fry', 'Leela', 'Scruffy', 'Dr. Zoidburg', 'Amy', 'Calculon']
-
-#our method
-
-50.times do |i|
-	alien[i] = Futurama_Aliens.new
-	alien[i].intro(chars.sample)
-	alien[i].location('earth', 100)
-end
-
-p alien.length
-
-#alternative method
-
-alien.each do |char_name|
-	char_name.intro(chars.sample)
-	char_name.location('mars', 0.2)
-end
-
-
-
-
