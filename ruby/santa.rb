7c319dbe6ed9a52320f3ff282cfacc0804c407d2
Davis Bare <davisbare409@gmail.com>
2016-11-30 14:59:50 -0800

    finished final loop, cannot get proper attributes to print out correctly tho

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,38 +1,50 @@
-class Santa
-	attr_accessor :gender, :age, :ethnicity
+number_of_santas = 0
 
-	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance...."
-		@age = 0
-		@ethnicity = ethnicity
-		@example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-		@example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-		@gender = gender
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
-	def speak
-		puts "Ho, ho, ho! Happy holidays!"
-	end
-	def eat_milk_and_cookies(flavor)
-		puts "That was a good #{flavor} cookie!"
-	end
-	def celebrate_birthday(current_age)
-		@celebrate_birthday = current_age + 1
-		p @celebrate_birthday
-	end
-	def get_mad_at=(reindeer_name)
-			@get_mad_at = @reindeer_ranking.last(reindeer_name)
-			p @get_mad_at
-	end
-end
+until number_of_santas == 50 do
+	class Santa
+		attr_reader :gender, :age, :ethnicity
+		#attr_accessor :gender, :age, :ethnicity
 
+		def initialize(gender, ethnicity)
+			example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+			example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+			
+			puts "Initializing Santa instance...."
+			@age = 0
+			@ethnicity = example_ethnicities[rand(example_ethnicities.length)]
+			@gender = example_genders[rand(example_genders.length)]
+			@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		end
+		def speak
+			puts "Ho, ho, ho! Happy holidays!"
+		end
+		def eat_milk_and_cookies(flavor)
+			puts "That was a good #{flavor} cookie!"
+		end
+		def celebrate_birthday(current_age)
+			@celebrate_birthday = current_age + 1
+			p @celebrate_birthday
+		end
+		def get_mad_at=(reindeer_name)
+				@get_mad_at = @reindeer_ranking.last(reindeer_name)
+				p @get_mad_at
+		end
+		puts "This Santa is #{@age} years old and is a great Santa!"
+		puts "They are #{@ethnicity}."
+		puts "When it comes to gender they prefer: #{@gender}."
+	end
+	number_of_santas += 1
+end 
+
+santa = Santa.new(' ',' ')
+
+=begin
 santa = Santa.new("male", "black")
 santa.speak
 santa.eat_milk_and_cookies("Oatmeal Raisin")
 santa.celebrate_birthday(24)
 #santa.get_mad_at("Dasher")  ---> not entirely sure why this isnt working
 
-=begin
 santas = []
 santas << Santa.new("agender", "black")
 santas << Santa.new("female", "Latino")
