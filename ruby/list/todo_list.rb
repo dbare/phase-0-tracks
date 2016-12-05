b3d8ea59d1f9c07ba44f30572fe15b4cad53b696
Davis Bare <davisbare409@gmail.com>
2016-12-04 21:22:15 -0800

    finished assignment

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,22 +1,21 @@
 class TodoList
-    def initialize(chore_list)
-        @item_list = chore_list
-    end
+  def initialize(chore_list)
+    @item_list = chore_list
+  end
 
-    def get_items
-        @item_list
-    end
+  def get_items
+    @item_list
+  end
 
-    def add_item(item)
-        @item_list << item
-    end
+  def add_item(item)
+    @item_list << item
+  end
 
-    def delete_item(item)
-        @item_list.delete(item)
-    end
+  def delete_item(item)
+    @item_list.delete(item)
+  end
 
-    def get_item(index)
-        @item_list[index]
-    end
-
+  def get_item(index)
+    @item_list[index]
+  end
 end
